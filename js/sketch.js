fda4e98827188d6a9df760ecbbdec7c0c1608785
Clémence .B <clembussiere@hotmail.fr>
2017-02-09 10:41:21 +0100

    update jpo

--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,87 @@
-function setup(){
-  createCanvas(500,500);
-  background(233);
+var remoteMouseX, remoteMouseY, ranger;
+var socket;
+var track,
+    mic,
+    fft,
+    spectrum,
+    waveform,
+    peaks,
+    amp;
+
+function setup() {
+	  createCanvas( windowWidth, windowHeight );
+
+	  masterVolume( 1 );
+
+	mic = new p5.AudioIn()
+	mic.start();
+
+	fft = new p5.FFT();
+	fft.setInput( mic );
+	// peaks = track.getPeaks( track.duration() * 10 );
+
+	// amplitude = new p5.Amplitude();
+	// amplitude.setInput( track );
+	textAlign( RIGHT );
+
+	socket = io.connect("http://172.16.128.119:8000");
+
+	socket.on( 'connected', function( data ){
+		document.querySelector( 'p' ).innerText = data.msg;
+	} );
+
+	// RECEIVE MOUSE POSITION
+	socket.on( 'mouse', function( data ){
+	      //console.log( JSON.stringify(data) );
+		    document.getElementById( 'mouseData' ).innerText = JSON.stringify(data);
+	      remoteMouseX = data.mouseX;
+	      remoteMouseY = data.mouseY;
+	} );
+
+	// RECEIVE RANGER POSITION
+	socket.on( 'ranger', function( data ){
+	      //console.log( JSON.stringify(data) );
+		    document.getElementById( 'rangerData' ).innerText = JSON.stringify(data);
+	      ranger = data;
+	} );
 }
+
+
+
+// function draw() {
+//   background( 255 );
+//
+//   fill( 0 );
+//   noStroke();
+//   text( "spectrum", width - 10, 100 );
+//   stroke( 0 );
+//   spectrum = fft.analyze();
+//   for( var i = 0; i < spectrum.length; i ++ ){
+//     line( i, height / 4, i, height / 4 - spectrum[ i ] );
+//   }
+//
+//   noStroke();
+//   text( "waveform", width - 10, height/4 + 100 );
+//   stroke( 0 );
+//   waveform = fft.waveform();
+//   for( var i = 0; i < waveform.length; i ++ ){
+//     line( i, height / 2, i, height / 2 + waveform[ i ] * 200 );
+//   }
+//
+//
+//   noStroke();
+//   text( "volume", width - 10, 3 * height/4 + 100 );
+//   stroke( 0 );
+//   var level = mic.getLevel();
+//   rect( 0, height, width, -level * 300 );
+//
+// }
 
 function draw(){
 	background(233);
   stroke("#8AB69A");
   noFill();
+	translate(350,100);
 
   rotation();
 
@@ -86,46 +161,37 @@ function draw(){
 
   }
 
-
-// function motif(){ //le 1er motif, triangle équilatéral
-//     triangle(90,340,249,63,410,340);//le premier triangle du pattern
-//
-//     translate(391,105);
-//     rotate(radians(90));
-//     triangle(90/1.75,340/1.75,249/1.75,63/1.75,410/1.75,340/1.75);//le second triangle du pattern à l'interieur du premier
-
-// triangle(taille/2,taille*1.5,taille*1.25,taille/4,taille*2,taille*1.5)
-
-// }
-
   function rotation(){
 
     push();
-    var distance = dist(mouseX, mouseY, 200, 200);
-    if (distance < 150) {
-    rotate(radians(frameCount));
-    }
+      rotate(radians(ranger*2));
     triangle(90,340,249,63,410,340);
     pop();
 
 
     push();
-    var distance = dist(mouseX, mouseY, 250, 250);
-    if (distance < 50) {
-    rotate(radians(frameCount));
-    }
+    rotate(radians(ranger/1.2));
     line(248.5,63,249,185);
     line(408,338.5,302,277);
     line(90,340,196,277);
     pop()
 
     push();
-    var distance = dist(mouseX, mouseY, 200, 250);
-    if (distance < 50) {
-    rotate(radians(frameCount));
-    }
+    rotate(radians(ranger));
     line(197,339,143,247);
     line(302,339,355,247);
     line(197,155,302,155);
     pop();
+
+		// prendre toutes les valeurs de son
+		var vol = mic.getLevel();
+
+		// Draw an ellipse with height based on volume
+		h = map(ranger, 0, 1, 100, 0);
+		ellipse(width/2, height/2, h*2, h*2);
+}
+
+
+function windowResized() {
+  resizeCanvas( windowWidth, windowHeight );
 }
